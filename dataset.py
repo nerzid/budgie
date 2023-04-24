da91c3ba435a5bdac2076149d6427dea5a72690c
nerzid <nerzid@gmail.com>
2023-04-24 15:10:41 +0000

    changed experimental code

--- dataset.py
+++ dataset.py
@@ -3,34 +3,34 @@ import json
 import spacy
 from spacy import displacy
 
-data_path = 'C:/Users/eyildiz/Desktop/Datasets/MULTIWOZ2.4/data.json'
+# data_path = 'C:/Users/eyildiz/Desktop/Datasets/MULTIWOZ2.4/data.json'
 nlp = spacy.load('en_core_web_sm')
 
 
-def read_file(num_lines):
-    result = ""
-
-    f = open(data_path)
-    data = json.load(f)
-
-    for i in data["SNG01856.json"]["log"]:
-        tokens = nlp(i['text'])
-        print('Sentence: ' + str(i))
-        for token in tokens:
-            if str(token.tag_).startswith("VB"):
-                print("Token:" + token.text + " POSTag: " + token.pos_ + " Tag: " + token.tag_ + " Morph:" + str(
-                    token.morph))
-        print('\n')
+# def read_file(num_lines):
+#     result = ""
+#
+#     f = open(data_path)
+#     data = json.load(f)
+#
+#     for i in data["SNG01856.json"]["log"]:
+#         tokens = nlp(i['text'])
+#         print('Sentence: ' + str(i))
+#         for token in tokens:
+#             if str(token.tag_).startswith("VB"):
+#                 print("Token:" + token.text + " POSTag: " + token.pos_ + " Tag: " + token.tag_ + " Morph:" + str(
+#                     token.morph))
+#         print('\n')
 
 
 if __name__ == '__main__':
     # print(read_file(5))
 
-    text = "I have not eaten that hamburger at Google yet."
-    # tokens = nlp(text)
-    doc = nlp(text)
+    text = "I'd like to eat a hamburger"
+    tokens = nlp(text)
+    # doc = nlp(text)
 
-    # for token in tokens:
-    #     print("Text:" + token.text + " POSTag: " + token.pos_ + " Tag: " + token.tag_)
-    #     print(
-    #         f'{token.text:{8}} {token.pos_:{6}} {token.tag_:{6}} {token.dep_:{6}} {spacy.explain(token.pos_):{20}} {spacy.explain(token.tag_)}')
+    for token in tokens:
+        print("Text:" + token.text + " POSTag: " + token.pos_ + " Tag: " + token.tag_)
+        print(
+            f'{token.text:{8}} {token.pos_:{6}} {token.tag_:{6}} {token.dep_:{6}} {spacy.explain(token.pos_):{20}} {spacy.explain(token.tag_)}')
--- parlai_denemeleri.py
+++ parlai_denemeleri.py
@@ -1,4 +1,5 @@
 from parlai.core.agents import Agent
+from parlai.core.opt import Opt
 from parlai.core.params import ParlaiParser
 from parlai.core.worlds import create_task
 
@@ -15,23 +16,26 @@ class RepeatLabelAgent(Agent):
 
     # return label from before if available
     def act(self):
-        reply = {'id': self.observation}
         if 'label_candidates' in self.observation:
-            print('/n')
+            print('\n')
             print(len(self.observation['label_candidates']))
-            print('/n')
+            print('\n')
+        else:
+            '\nNo label candidates: '.join(self.observation['text']).join('\n')
 
-        return reply
+        return self.observation
 
 
 if __name__ == '__main__':
     parser = ParlaiParser()
     opt = parser.parse_args()
-
+    # opt = ['--task', 'light_dialog']
+    # opt = Opt()
+    # opt['task'] = 'light_dialog'
     agent = RepeatLabelAgent(opt)
     world = create_task(opt, agent)
 
-    for _ in range(1):
+    for _ in range(3):
         world.parley()
         for a in world.acts:
             # print the actions from each agent
