a533410914839e5591ae03114f0232da15797333
nerzid <nerzid@gmail.com>
2024-05-13 11:30:59 +0200

    any information now has negation.any instead of false as default

--- socialds/any/any_information.py
+++ socialds/any/any_information.py
@@ -3,9 +3,9 @@ from socialds.any.any_object import AnyObject
 from socialds.any.any_property import AnyProperty
 from socialds.enums import Tense
 from socialds.socialpractice.context.information import Information
-from socialds.states.relation import RType
+from socialds.states.relation import Negation, RType
 
 
 class AnyInformation(Information, AnyObject):
     def __init__(self):
-        super().__init__(AnyObject(), RType.ANY, Tense.ANY, AnyProperty())
+        super().__init__(AnyObject(), RType.ANY, Tense.ANY, AnyProperty(), Negation.ANY)
